08744ac2f00ffc6fcc22d653c0404029b4f304ea
bryce <bryce@unsupervised.com>
2019-03-16 20:11:06 -0600

    brute force tic tac toe

--- yomi/agents/__init__.py
+++ yomi/agents/__init__.py
@@ -1,7 +1,9 @@
 from .random_agent import RandomAgent
 from .human_agent import HumanAgent
 from .silly_tic_tac_toe import SillyAgent
+from .brute_force_tic_tac_toe import BruteForceTicTacToe
 
 __all__ = ['RandomAgent',
            'HumanAgent',
-           'SillyAgent']
+           'SillyAgent',
+           'BruteForceTicTacToe']
